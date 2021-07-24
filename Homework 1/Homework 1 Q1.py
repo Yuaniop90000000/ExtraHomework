6597ba98abb791926b82c502c972122425c79834
Yuaniop90000000 <87884312+Yuaniop90000000@users.noreply.github.com>
2021-07-24 12:38:24 -0400

    #Yuan_H1Q1
    r=2
    pi=3.14159
    print("diameter =",2*r,", circumference =",2*pi*r, ", area =",pi*r**2)

--- Homework 1/Homework 1 Q1.py
+++ Homework 1/Homework 1 Q1.py
@@ -5,3 +5,7 @@ pi = 3.14159;
 print('diameter =', 2*r);
 print('circumference =', 2*pi*r);
 print('area =', pi*(r**2));
+
+r=2
+pi=3.14159
+print("diameter =",2*r,", circumference =",2*pi*r, ", area =",pi*r**2)
